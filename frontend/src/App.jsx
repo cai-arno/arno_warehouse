c39fa96e7c43e66114a716810881f1672bc1adbc
cai-arno <arno.cai@outlook.com>
2026-04-14 18:53:24 +0800

    Add frontend/src/App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,8 @@ import WrongQuestionBook from './pages/WrongQuestionBook';
 import Profile from './pages/Profile';
 import Achievement from './pages/Achievement';
 import Diagnosis from './pages/Diagnosis';
+import Membership from './pages/Membership';
+import EyeCareReminder from './components/EyeCareReminder';
 
 // 路由守卫
 function PrivateRoute({ children }) {
@@ -39,29 +41,41 @@ function PublicRoute({ children }) {
   return user ? <Navigate to="/home" /> : children;
 }
 
+function AppContent() {
+  return (
+    <>
+      <Routes>
+        {/* 公开路由 */}
+        <Route path="/login" element={<PublicRoute><Login /></PublicRoute>} />
+        
+        {/* 私有路由 */}
+        <Route path="/home" element={<PrivateRoute><Home /></PrivateRoute>} />
+        <Route path="/junior" element={<PrivateRoute><JuniorSection /></PrivateRoute>} />
+        <Route path="/senior" element={<PrivateRoute><JuniorSection /></PrivateRoute>} />
+        <Route path="/practice" element={<PrivateRoute><QuestionPractice /></PrivateRoute>} />
+        <Route path="/analysis" element={<PrivateRoute><AnswerAnalysis /></PrivateRoute>} />
+        <Route path="/wrong" element={<PrivateRoute><WrongQuestionBook /></PrivateRoute>} />
+        <Route path="/profile" element={<PrivateRoute><Profile /></PrivateRoute>} />
+        <Route path="/achievement" element={<PrivateRoute><Achievement /></PrivateRoute>} />
+        <Route path="/diagnosis" element={<PrivateRoute><Diagnosis /></PrivateRoute>} />
+        <Route path="/membership" element={<PrivateRoute><Membership /></PrivateRoute>} />
+        
+        {/* 默认跳转 */}
+        <Route path="/" element={<Navigate to="/home" />} />
+        <Route path="*" element={<Navigate to="/home" />} />
+      </Routes>
+      
+      {/* 全局护眼提醒 */}
+      <EyeCareReminder enabled={true} intervalMinutes={45} />
+    </>
+  );
+}
+
 export default function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
-        <Routes>
-          {/* 公开路由 */}
-          <Route path="/login" element={<PublicRoute><Login /></PublicRoute>} />
-          
-          {/* 私有路由 */}
-          <Route path="/home" element={<PrivateRoute><Home /></PrivateRoute>} />
-          <Route path="/junior" element={<PrivateRoute><JuniorSection /></PrivateRoute>} />
-          <Route path="/senior" element={<PrivateRoute><JuniorSection /></PrivateRoute>} />
-          <Route path="/practice" element={<PrivateRoute><QuestionPractice /></PrivateRoute>} />
-          <Route path="/analysis" element={<PrivateRoute><AnswerAnalysis /></PrivateRoute>} />
-          <Route path="/wrong" element={<PrivateRoute><WrongQuestionBook /></PrivateRoute>} />
-          <Route path="/profile" element={<PrivateRoute><Profile /></PrivateRoute>} />
-          <Route path="/achievement" element={<PrivateRoute><Achievement /></PrivateRoute>} />
-          <Route path="/diagnosis" element={<PrivateRoute><Diagnosis /></PrivateRoute>} />
-          
-          {/* 默认跳转 */}
-          <Route path="/" element={<Navigate to="/home" />} />
-          <Route path="*" element={<Navigate to="/home" />} />
-        </Routes>
+        <AppContent />
       </BrowserRouter>
     </AuthProvider>
   );
